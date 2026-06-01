d907cff16111e638402a577ac175557679e3f620
lxuiejane <9023130@student.zadkine.nl>
2026-06-01 18:56:20 +0200

    fonts import, SEDGWICK AND AFACAD.

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,33 @@
 import Navbar from "@/components/Navbar";
-import './globals.css';
+import "./globals.css";
+
+import {
+  Sedgwick_Ave_Display,
+  Afacad_Flux,
+} from "next/font/google";
+
+const sedgwick = Sedgwick_Ave_Display({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-sedgwick",
+});
+
+const afacad = Afacad_Flux({
+  subsets: ["latin"],
+  variable: "--font-afacad",
+});
 
 export const metadata = {
-  title: 'Barbershop',
-  description: 'book all your appointments online',
+  title: "Barbershop",
+  description: "book all your appointments online",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="nl">
+    <html
+      lang="nl"
+      className={`${sedgwick.variable} ${afacad.variable}`}
+    >
       <body suppressHydrationWarning>
         <Navbar />
         {children}
