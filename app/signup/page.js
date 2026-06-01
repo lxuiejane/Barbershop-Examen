44ee51e193e928c31c587eefd0956ea63837fbc6
lxuiejane <9023130@student.zadkine.nl>
2026-06-01 15:38:08 +0200

    fixed.

---
+++ app/signup/page.js
@@ -0,0 +1,9 @@
+import RegisterForm from "../components/RegisterForm";
+
+export default function SignupPage() {
+  return (
+    <main className="registerPage">
+      <RegisterForm />
+    </main>
+  );
+}
