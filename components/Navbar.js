a0d6d0fc77557109d6134a679ac248c296846a53
lxuiejane <9023130@student.zadkine.nl>
2026-06-02 16:22:21 +0200

    navbar fix for sign in and sign up

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,34 @@
 import Link from "next/link";
 
 export default function Navbar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="logo">
-                    <img src="/logo2.png" alt="Urban Edge" />
-                </div>
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <img src="/logo2.png" alt="Urban Edge" />
+        </div>
 
-                <ul className="navLinks">
-                    <li><Link href="/">Services</Link></li>
-                    <li><Link href="/profile">Profile</Link></li>
-                    <li><Link href="/contact">Contact</Link></li>
-                </ul>
-
-                <button className="signOut">Sign Out</button>
-
-            </nav>
-        </>
-    );
+        <ul className="navLinks">
+          <li>
+            <Link href="/">Services</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+        <ul className="authLinks">
+            <li>
+                <Link href="/signin">Sign In</Link>
+            </li>
+            <p>|</p>
+            <li>
+                <Link href="/signup">Sign Up</Link>
+            </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
